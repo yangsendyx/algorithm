18809c702b1e285bb37715858c2984fea3ec36fc
yangsen <sen@neradigm.com>
2019-09-24 18:13:16 +0800

    简化下fileOperation.js代码

--- 02/js/FileOperation.js
+++ 02/js/FileOperation.js
@@ -6,19 +6,12 @@ module.exports = async path => new Promise((resolve, reject) => {
 		const words = [];
 		const rs = fs.createReadStream(path);
 		const rl = readline.createInterface({ input: rs });
-		const cb = line => {
-			const list = line.split(/\s+/g);
-			list.forEach(item => {
-				if( !item ) return;
-				const arr = item.split(/[^a-zA-Z]/g);
-				if( !arr.length ) return;
-				
-				arr.forEach(word => {
-					if( !word ) return;
-					words.push( word.toLocaleLowerCase() );
-				});
-			});
-		};
+		const cb = line => line.split(/\s+/g).forEach(item => (
+				!!item && item.split(/[^a-zA-Z]/g).forEach(word => (
+					!!word && words.push(word.toLocaleLowerCase())
+				))
+			)
+		);
 
 		rl.on('line', cb);
 		rl.on('close', () => resolve(words));
